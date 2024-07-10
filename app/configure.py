fb11f6753a97902317f2024f5503efc4216548d4
srikanth <banothusrikanth267@gmail.com>
2024-07-10 11:09:18 +0530

    added

--- app/configure.py
+++ app/configure.py
@@ -1,7 +1,7 @@
 from decouple import config
 class Config:
     SECRET_KEY ="secret-key"
-    SQLALCHEMY_DATABASE_URI = config("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = "mysql://avnadmin:AVNS_mEQfCEIkupo_GdLjlhc@mysql-323302ad-banothusrikanth267-d588.a.aivencloud.com:26621/defaultdb"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDERS = {
         'python': {
--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 
 from app.__init__ import create_app
-from dotenv import load_dotenv
 
-load_dotenv()
 
 app = create_app()
 
